46f3b8a90b038b825f1bf1ded96892b10bf2a518
Aaron Marriner <amarriner@amarriner.com>
2020-06-30 17:00:30 -0400

    Testing command

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const discord = require("./discord.js");
 const characters = require("./characters.json");
 const users = require("./users.json");
 
+const commandPrefix = "_";
+
 discord.client.on('ready', () => {
     console.log('I am ready!');
 });
 
 discord.client.on("message", message => {
 
-   if (message.content.startsWith("!whatismy")) {
+   if (message.content.startsWith(commandPrefix + "attr")) {
+      var [command, attribute] = message.content.split(" ");
+      console.log(command + " " + attribute + " " + message.author.id + " " + characters[message.author.id][0].attributes[attribute]);
    }
 
 });
