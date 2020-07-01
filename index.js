a8a0771e8e83b22e2fbc17ce1aae1922340edfab
Aaron Marriner <amarriner@amarriner.com>
2020-07-01 17:17:36 -0400

    Change color if roll fails

--- index.js
+++ index.js
@@ -88,6 +88,9 @@ discord.client.on("message", message => {
 
       var diceRollResult = utils.rollDice(dice);
       r.message.timestamp = new Date();
+      if ((parseInt(parseInt(diceRollResult[0])) > parseInt(value))) {
+         r.message.color = 15158332;
+      }
       r.message.footer = {};
       r.message.footer.text = "Roll result: " + ((diceRollResult.length > 1) ? diceRollResult.join(", ") : diceRollResult[0]);
       r.message.footer.icon_url = (parseInt(parseInt(diceRollResult[0])) <= parseInt(value)) ? config.rollSuccessUrl : config.rollFailureUrl;
